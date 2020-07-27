820a3be23bd9e30090b5d775153e05bce9de1c4e
009kings <009kings@gmail.com>
2020-07-27 10:49:20 -0700

    models are always singular

--- mern-auth-backend/config/passport.js
+++ mern-auth-backend/config/passport.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
-const User = mongoose.model('users');
+const User = mongoose.model('User');
 
 const options= {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
